324c8e3aff670b6853e4a8a97de4f6c437f66dc6
code1009 <code1009@naver.com>
2025-07-08 08:59:11 +0900

    1

--- practice_git/run2.cpp
+++ practice_git/run2.cpp
@@ -2,7 +2,7 @@
 
 void run1()
 {
-    std::cout << "run2 입니다.\n";
-    std::cout << "run2 입니다.\n";
-    std::cout << "run2 입니다.\n";
+    std::cout << "run2 수정 입니다.\n";
+    std::cout << "run2 수정 입니다.\n";
+    std::cout << "run2 수정 입니다.\n";
 }
